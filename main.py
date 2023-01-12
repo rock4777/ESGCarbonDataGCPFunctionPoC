1522aba5f670e2b5ea0e0566cfc63e0e9711209e
Dan Smirnov <dansmirnov47@gmail.com>
2023-01-12 08:56:15 -0500

    Add GCP function framework

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+from google.cloud import functions_framework
 import config
 import download as dl
 import gcp_util as gcp
 
-
-dl.getESGData(config.url, config.dataFilter, config.headers)
-df = dl.normESGData()
-fileName = dl.exportESGtoCSV(df)
-fileName = 'esg_clean_carbon_2023_01_11.csv'
-gcp.upload_to_bucket(config.bucketName, fileName, fileName)
-gcp.loadIntoBigQuery(fileName)
+@functions_framework.http
+def esg_etl(request):
+    dl.getESGData(config.url, config.dataFilter, config.headers)
+    df = dl.normESGData()
+    fileName = dl.exportESGtoCSV(df)
+    fileName = 'esg_clean_carbon_2023_01_11.csv'
+    gcp.upload_to_bucket(config.bucketName, fileName, fileName)
+    gcp.loadIntoBigQuery(fileName)
